68b5e83dab78fbbb18b2bb5bea1aa9edeb8a7699
Bingyi Liu <31932950+zhezixi@users.noreply.github.com>
2020-04-17 23:31:01 -0400

    Update challenge5.js

--- js/challenge5.js
+++ js/challenge5.js
@@ -1,8 +1,26 @@
 $(document).ready(function(){
-  $(this).mouseover(function(){
+
+  $("img").mouseover(function(){
     $("#display").css("background-image", "url('" + this.src + "')");
-    $("#display").text($(this).attr("alt"));
+    $("#display").text(this.alt);
     console.log("image hovered");
   });
 
+  $("img").mouseleave(function(){
+    $("#display").css("background-image", "url('')");
+    $("#display").text("Hover over an image below to display the image and the alt text.");
+    console.log("image left");
+  });
+
+  $("img").focus(function(){
+    $("#display").css("background-image", "url('" + this.src + "')");
+    $("#display").text(this.alt);
+    console.log("focus");
+  });
+
+  $("img").blur(function(){
+    $("#display").css("background-image", "url('')");
+    $("#display").text("Hover over an image below to display the image and the alt text.");
+    console.log("blur");
+  });
 });
